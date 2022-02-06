cbceb6ad15f6dfaf74fcb040c5a9b786e62af975
HelQ <ekinto88@gmail.com>
2022-02-06 23:20:52 +0100

    Paging

---
+++ client/src/components/Pages.js
@@ -0,0 +1,28 @@
+import React, {useContext} from 'react';
+import {observer} from "mobx-react-lite";
+import {Context} from "../index";
+import {Pagination} from "react-bootstrap";
+
+const Pages = observer(() => {
+    const {users} = useContext(Context)
+    const pageCount = Math.ceil(users.totalCount / users.limit)
+    const pages = []
+    for (let i = 0; i < pageCount; i++){
+        pages.push(i + 1)
+    }
+    return (
+        <Pagination className="m-md-2">
+            {pages.map(page =>
+                <Pagination.Item
+                    key = {page}
+                    active = {users.page === page}
+                    onClick = {() => users.setPage(page)}
+                >
+                    {page}
+                </Pagination.Item>
+            )}
+        </Pagination>
+    );
+});
+
+export default Pages;
